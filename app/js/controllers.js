ca2601bda5b3d59fd0ba2615641f1e8084681518
Youngbye <ybooty0@gmail.com>
2015-11-23 00:23:56 +0800

    add bootstrap, add sass

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,7 +8,6 @@ cnodeAppCtrl.controller('getTopicsCtrl', ['$scope', '$http',
     function($scope, $http) {
         $http.get('https://cnodejs.org/api/v1/topics?mdrender=false').success(function(data) {
             $scope.topics = data.data;
-            console.log($scope.topics.data);
         });
     }
 ]);
