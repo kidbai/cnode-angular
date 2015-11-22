1c7410ec26e2481ccddb8e037017e49b2ab146c3
Youngbye <ybooty0@gmail.com>
2015-11-22 23:31:46 +0800

    更新topics view

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,9 +6,9 @@ var cnodeAppCtrl = angular.module('cnodeAppCtrl', []);
 
 cnodeAppCtrl.controller('getTopicsCtrl', ['$scope', '$http',
     function($scope, $http) {
-        $http.get('https://cnodejs.org/api/v1/topics').success(function(data) {
-            $scope.topics = data;
-            console.log(data);
+        $http.get('https://cnodejs.org/api/v1/topics?mdrender=false').success(function(data) {
+            $scope.topics = data.data;
+            console.log($scope.topics.data);
         });
     }
 ]);
