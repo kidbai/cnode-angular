d12bb1d8340ce8b1c79a084039121046e279dc2f
Youngbye <ybooty0@gmail.com>
2015-12-31 14:36:34 +0800

    modify style

--- app/js/app.js
+++ app/js/app.js
@@ -3,29 +3,30 @@
 /* App Module */
 
 var cnodeApp = angular.module('cnodeApp', [
-  'ui.router',
-  'ui.bootstrap',
-  'cnodeAppCtrl',
-  'ngAnimate'
+    'ui.router',
+    'ui.bootstrap',
+    'cnodeAppCtrl',
+    'ngAnimate'
 ]);
 
-cnodeApp.config(function($stateProvider, $urlRouterProvider){
-  $urlRouterProvider.otherwise("/topics");
+cnodeApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/topics");
 
-  $stateProvider
-    .state('topics', {
-      url: "/topics",
-      views: {
-        "": {
-          templateUrl: "partials/topics.html",
-        },
-        "navbar": {
-          templateUrl: "partials/navbar.html"
-        }
-      }
-    })
-    .state('topic', {
-        url: '/topic/:topicId',
-        templateUrl: 'partials/topic.html'
-    })
+    $stateProvider
+        .state('topics', {
+            url: "/topics:tab?",
+            views: {
+                "content": {
+                    templateUrl: "partials/topics.html"
+                }
+            }
+        })
+        .state('home.navbar',{
+            templateUrl: 'partials/navbar.html'
+        })
+        .state('topic', {
+            url: '/topic/:topicId',
+            templateUrl: 'partials/topic.html'
+        })
 })
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,18 +4,30 @@
 
 var cnodeAppCtrl = angular.module('cnodeAppCtrl', ['ngSanitize']);
 
-cnodeAppCtrl.controller('getTopicsCtrl', ['$scope', '$http', '$sce',
-    function($scope, $http, $sce) {
-        $http.get('https://cnodejs.org/api/v1/topics').success(function(data) {
-            $scope.topics = data.data;
-            console.log($scope.topics);
-        });
+cnodeAppCtrl.controller('getTopicsCtrl', ['$scope', '$http', '$sce', '$stateParams',
+    function($scope, $http, $sce, $stateParams) {
+        console.log($stateParams);
+        if($stateParams.tab === ''){
+            $http.get('https://cnodejs.org/api/v1/topics').success(function(data) {
+                $scope.topics = data.data;
+                console.log($scope.topics);
+            });
+        }else{
+            $http.get('https://cnodejs.org/api/v1/topics' + $stateParams.tab).success(function(data) {
+                $scope.topics = data.data;
+                console.log($scope.topics);
+            });
+        }
+        
         $scope.kindOfTab = function(value) {
             if(value === 'ask'){
                 return '问答';
             }
             if(value === 'share'){
                 return '分享';
+            }
+            if(value === 'job'){
+                return '工作';
             }
 
         }
@@ -34,3 +46,9 @@ cnodeAppCtrl.controller('getTopicCtrl', ['$scope', '$http', '$sce', "$stateParam
         }
     }
 ]);
+
+cnodeAppCtrl.controller('sidebarCtrl', ['$scope',
+    function($scope){ 
+        $scope.sidebarState = false;
+    }
+])
