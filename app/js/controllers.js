f005eeec3cd2607913f32402d9b30f57da2dc8f7
Youngbye <ybooty0@gmail.com>
2016-01-04 16:45:57 +0800

    分类样式处理

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,25 +7,41 @@ var cnodeAppCtrl = angular.module('cnodeAppCtrl', ['ngSanitize']);
 cnodeAppCtrl.controller('getTopicsCtrl', ['$scope', '$http', '$sce', '$stateParams',
     function($scope, $http, $sce, $stateParams) {
         console.log($stateParams.tab);
-        $http.get('https://cnodejs.org/api/v1/topics/?tab=' + $stateParams.tab + '&limit=20').success(function(data) {
+        $http.get('https://cnodejs.org/api/v1/topics/?tab=' + $stateParams.tab + '&limit=40').success(function(data) {
             $scope.topics = data.data;
             console.log($scope.topics);
             var topics = $scope.topics;
             angular.forEach(topics, function(value, key){
-                console.log(value.tab);
-                console.log(key);
-                value.isAsk = false;
-                value.isShare = false;
-                value.isJob = false;
-                switch(value.tab)
-                {
-                    case "ask": value.tab = '问答'; value.isAsk = true; value.isShare = false; value.isJob = false; break;
-                    case "share": value.tab = '分享'; value.isAsk = false; value.isShare = true; value.isJob = false;break;
-                    case "job": value.tab = '工作'; value.isAsk = false; value.isShare = false; value.isJob = true;break;
-                    case undefined: value.tab = '暂无'; value.isAsk = false; value.isShare = false; value.isJob = false; break;
-                    default:
-                        break;
+                value.label = '';
+                if(value.top){
+                    value.label = 'top';
+                    value.newTab = '顶置';
                 }
+                if(!value.top && value.good){
+                    value.label = 'good';
+                    value.newTab = '精华';
+                }
+                if(!value.top && !value.good && value.tab != ''){
+                    value.label = value.tab;
+                     switch(value.tab)
+                    {
+                        case "ask": value.newTab = '问答'; break;
+                        case "share": value.newTab = '分享'; break;
+                        case "job": value.newTab = '招聘'; break;
+                        case undefined: value.newTab = '暂无'; break;
+                        default:
+                            break;
+                    }
+                }
+                if(!value.top && !value.good && value.tab === undefined){
+                    value.label = "none";
+                    value.newTab = '暂无';
+                }
+                console.log(value.label);
+                // value.isAsk = false;
+                // value.isShare = false;
+                // value.isJob = false;
+               
             })
         });
         
